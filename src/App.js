bc0f855fac0db296cb1c343a703e2872531bb703
pguyon <pguyon69@gmail.com>
2022-09-24 12:36:39 +0200

    create Home and Login pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-        <Routes></Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </Router>
     </>
   );
