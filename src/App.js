182388864fc881b87019a8c36ddad6f4a0e2422c
pguyon <pguyon69@gmail.com>
2022-09-24 14:34:58 +0200

    fix sry-only into sr-only in Home.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
