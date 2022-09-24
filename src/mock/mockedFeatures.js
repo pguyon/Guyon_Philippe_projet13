e72af738ab06de7652a91f8d766947921e577c54
pguyon <pguyon69@gmail.com>
2022-09-24 08:41:10 +0200

    add assets and mocked data

---
+++ src/mock/mockedFeatures.js
@@ -0,0 +1,26 @@
+export const mockedFeatures = [
+  {
+    id: 1,
+    img: "pictureChat",
+    sourceimg: "../../assets/icon-chat.png",
+    altimg: "picture of chat",
+    title: "You are our #1 priority",
+    text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+  },
+  {
+    id: 2,
+    img: "pictureMoney",
+    sourceimg: "../../assets/icon-money.png",
+    altimg: "picture of money",
+    title: "More savings means higher rates",
+    text: "The more you save with us, the higher your interest rate will be!",
+  },
+  {
+    id: 3,
+    img: "pictureSecurity",
+    sourceimg: "../../assets/icon-security.png",
+    altimg: "picture of security",
+    title: "Security you can trust",
+    text: "We use top of the line encryption to make sure your data and money is always safe.",
+  },
+];
