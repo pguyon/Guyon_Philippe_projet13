95508ea746b1536ee487243cc61c475c55793c85
unknown <pguyon69@gmail.com>
2022-10-05 03:50:00 +0200

    update request

--- src/features/apiService.js
+++ src/features/apiService.js
@@ -7,39 +7,47 @@ const instance = axios.create({
 /**
  * This function sends a POST request to the server with the user's login credentials, and if the
  * response is successful, it saves the user's data to local storage.
- * @param {object} - userData 
+ * @param {string} - user's email
+ * @param {string} - user's password
  * @returns The response.data is being returned.
  */
-const userLogin = async (userData) => {
-  const response = await instance.post(`/login`, userData);
+const userLogin = async (email, password) => {
+  const response = await instance.post(`/login`, {
+    email,
+    password,
+  });
 
   if (response.ok) {
-    localStorage.setItem("argentBank", JSON.stringify(response.data));
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
   return response.data;
 };
 
 /**
  * It's a function that takes in two parameters, userDataProfile and token, and returns a response.data
- * object.
- * @param {object} - userProfileData
+ * object. 
  * @param {string} - the token that is returned from the login function
  * @returns The response.data is the data that is being returned from the server.
  */
-const userProfile = async (userProfileData, token) => {
+const userProfile = async ( token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
 
-  const response = await instance.put(`/profile`, userProfileData, config);
+  const response = await instance.post(`/profile`, config);
   return response.data.body;
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
 };
 
 const apiService = {
   userLogin,
   userProfile,
+  logout,
 };
 
 export default apiService;
