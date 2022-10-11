72689cdf3d8b240fd8aad99071588252fdd3a585
unknown <pguyon69@gmail.com>
2022-10-11 09:14:56 +0200

    add navigate to login if not logged on profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import { userProfile } from "../../services/authService";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
+  let navigate = useNavigate();
+  const isLogged = useSelector((state) => state.login.isLogged);
+  useEffect(() => {
+    if (!isLogged) return navigate("/login");
+  }, [isLogged, navigate]);
 
-  const token = localStorage.getItem('user')
-  console.log(token);
+
   // const firstName = useSelector((state) => state.user.firstName);
   // const lastName = useSelector((state) => state.user.lastName);
 
