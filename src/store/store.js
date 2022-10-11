b8ed6f4806da5190524dcf15efe0db974ca47f33
unknown <pguyon69@gmail.com>
2022-10-11 07:46:50 +0200

    add loginSlice on store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slice/userSlice";
-
+import { loginSlice } from "./slice/loginSlice";
 
 const store = configureStore({
-  reducer: { user: userSlice.reducer},
+  reducer: { user: userSlice.reducer, login: loginSlice.reducer },
 });
 
 export default store;
