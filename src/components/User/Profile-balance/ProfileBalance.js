e61ecaa59ef352e36510ceb6d7afaa4f5500fcc0
unknown <pguyon69@gmail.com>
2022-10-14 07:56:10 +0200

    add css on Profile.js

--- src/components/User/Profile-balance/ProfileBalance.js
+++ src/components/User/Profile-balance/ProfileBalance.js
@@ -4,12 +4,15 @@ import { mockedProfile } from "../../../mock/mockedProfile";
 
 const ProfileBalance = () => {
   return (
-    <section>
+    <section className={classes.main}>
       {mockedProfile.map((item) => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <p>{item.amount}</p>
-          <p>{item.amountDescription}</p>
+        <div key={item.id} className={classes['main-balance']}>
+          <div>
+            <p>{item.title}</p>
+            <p>{item.amount}</p>
+            <p>{item.amountDescription}</p>
+          </div>
+          <span className={classes.btn}>View transactions</span>
         </div>
       ))}
     </section>
