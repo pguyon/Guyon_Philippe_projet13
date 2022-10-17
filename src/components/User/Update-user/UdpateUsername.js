cd9cfeeb8ae8ba174381ffebef7816e269b41b89
unknown <pguyon69@gmail.com>
2022-10-17 14:37:43 +0200

    start UpdateUsername.js

--- src/components/User/Update-user/UdpateUsername.js
+++ src/components/User/Update-user/UdpateUsername.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import classes from './UdpateUsername.module.css'
+import React, { useState } from "react";
+import classes from "./UpdateUsername.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const UdpateUsername = () => {
-  return (
-    <div>UdpateUsername</div>
-  )
-}
+  const dispatch = useDispatch();
+  const firstname = useSelector((state) => state.user.firstName);
+  const lastname = useSelector((state) => state.user.lastName);
+  const { updateFirstname, setUpdateFirstname } = useState("");
+  const { updateLastname, setUpdateLastname } = useState("");
+  // const  = useSelector(state => state.user.email)
 
-export default UdpateUsername
+  const submitHandler = () => {};
+
+  const firstnameHandler = (e) => {
+    setUpdateLastname(e.target.value);
+  };
+
+  const lastnameHandler = (e) => {
+    setUpdateLastname(e.target.value);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes["input-wrapper"]}>
+        <input
+          type="text"
+          id="firstname"
+          onChange={firstnameHandler}
+          defaultValue={firstname}
+          label="Change firstname"
+        />
+      </div>
+      <div className={classes["input-wrapper"]}>
+        <input
+          type="text"
+          id="lastname"
+          onChange={lastnameHandler}
+          defaultValue={lastname}
+          label="Change lastname"
+        />
+      </div>
+
+      <button className={classes.btn} type="submit">
+        Save
+      </button>
+      <button className={classes.btn}>Cancel</button>
+    </form>
+  );
+};
+
+export default UdpateUsername;
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,21 @@
-import React from "react";
-import UserIdentity from "../../components/User/Identity/UserIdentity";
+import React, { useEffect } from "react";
 import ProfileBalance from "../../components/User/Profile-balance/ProfileBalance";
+import UdpateUsername from "../../components/User/Update-user/UdpateUsername";
 import classes from "./Profile.module.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
+  let navigate = useNavigate();
+  const isLogged = useSelector((state) => state.login.isLogged);
+  useEffect(() => {
+    if (!isLogged) return navigate("/login");
+  }, [isLogged, navigate]);
+
   return (
     <main className={classes.main}>
-      <UserIdentity />
+      <h2>Welcome back</h2>      
+      <UdpateUsername />
       <ProfileBalance />
     </main>
   );
